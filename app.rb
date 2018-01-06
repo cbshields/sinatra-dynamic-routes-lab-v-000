6ce19dad86a759681cd0839d602983bd999726f7
Caroline Shields <caroline.shields@amkor.com>
2018-01-06 21:35:53 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -16,6 +16,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @num = params[:number].to_i
     @phrase = params[:phrase]
+    binding.pry
     @num.times do
       "#{@phrase}"
     end
