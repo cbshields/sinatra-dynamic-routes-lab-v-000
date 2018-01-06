742bb710a44b3331b6e724a096d4fa3bc3e752b4
Caroline Shields <caroline.shields@amkor.com>
2018-01-06 21:28:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get '/reversename/:name' do
+    @name = params[:name].reverse
+    "#{@name}"
+  end
+
+  get '/square/:number' do
+    @num = params[:number].to_i ** 2
+    "#{@num.to_s}"
+  end
 end
