6b01bec8b91b8afb669e52ed0755d9f6d8bf841f
Caroline Shields <caroline.shields@amkor.com>
2018-01-06 21:58:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,4 +25,26 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{ params[:word5]}."
   end
 
+  get '/:operation/:number1/:number2' do
+    @math = params[:operation]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
+
+    if @math == "add"
+      @tot = @number1 + @number2
+      "#{tot}"
+    elsif @math == "subtract"
+      @tot = @number1 - @number2
+      "#{tot}"
+    elsif @math == "multiply"
+      @tot = @number1 * @number2
+      "#{tot}"
+    else
+      @tot = @number1 / @number2
+      "#{tot}"
+    end
+      
+    end
+  end
+
 end #ends Class app
